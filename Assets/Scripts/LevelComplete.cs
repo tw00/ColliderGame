c9cb87f88327b238e23ec6b281a52a3e88f4c9e6
Sebastian Cordes <sc@wavefab.com>
2019-10-31 21:38:33 +0100

    Add menu Mac some components more general

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -3,8 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
-    public void LoadNextLevel() {
-      // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    public void LoadNextLevel () {
+        // Check if current scene is the last one -> Navigate back to menu
+        int nextLevelBuildIndex = SceneManager.GetActiveScene ().buildIndex + 1;
+        if (nextLevelBuildIndex >= SceneManager.sceneCountInBuildSettings) {
+            SceneManager.LoadScene (0);
+            return;
+        }
+
+        // Get name of current scene and next scene
+        string sceneName = SceneManager.GetActiveScene ().name;
+        string nextSceneName = SceneManager.GetSceneByBuildIndex (nextLevelBuildIndex).name;
+
+        if (sceneName.Length > 7 && nextSceneName.Length > 7) {
+            // Check if name tag of scenes is equal
+            if (sceneName[5] == nextSceneName[5] || sceneName[6] == nextSceneName[6]) {
+                // Progress to next level
+                SceneManager.LoadScene (nextLevelBuildIndex);
+            }
+        }
+
+        // Go back to menu
+        SceneManager.LoadScene (0);
     }
 }
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartTwEpisodes() {
+        SceneManager.LoadScene("LevelTW01");
+    }
+
+    public void StartScEpisodes () {
+        SceneManager.LoadScene ("LevelSC01");
+    }
+}

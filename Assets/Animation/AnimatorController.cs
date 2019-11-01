cf01e6815d24e718fa16aaf19f9b650ee62f9f20
Thomas <tw@goalpilot.com>
2019-11-01 00:01:32 -0400

    :truck: renamed particles folder

--- Assets/Animation/AnimatorController.cs
+++ Assets/Animation/AnimatorController.cs
@@ -8,15 +8,9 @@ public class AnimatorController : MonoBehaviour
     public float offset = 0f;
     public Animator anim;
 
-    // Start is called before the first frame update
     void Start()
     {
       anim = GetComponent<Animator>();
       anim.SetFloat("offset", offset);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }

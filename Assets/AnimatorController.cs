25d73b8970c72dd6875213eed516bc453d166539
Thomas <tw@goalpilot.com>
2019-10-31 23:47:12 -0400

    :construction: more obstacles

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+
+    public float offset = 0f;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = GetComponent<Animator>();
+      anim.SetFloat("offset", offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

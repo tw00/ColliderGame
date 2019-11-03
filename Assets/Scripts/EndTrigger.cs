f68f499a0af68ec62e7d44e34c52fc07e2119617
Thomas <tw@goalpilot.com>
2019-11-03 02:38:14 -0500

    :art: end trigger can now find game manager if not set

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -5,6 +5,9 @@ public class EndTrigger : MonoBehaviour
     public GameManager gameManager;
 
     private void OnTriggerEnter(Collider other) {
+      if ( !gameManager ) {
+        gameManager = FindObjectOfType<GameManager>() as GameManager;
+      }
       gameManager.CompleteLevel();
     }
 }

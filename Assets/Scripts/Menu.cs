a2370ff9df877badca34b511db35b7daa3cfbbe1
Thomas <tw@goalpilot.com>
2019-11-03 02:37:06 -0500

    :art: exit game / toggle fullscreen in menu (not working)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
@@ -7,7 +8,36 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene("LevelTW01");
     }
 
+    public void StartInfiniteEpisodes()
+    {
+       SceneManager.LoadScene("LevelTW03");
+    }
+
     public void StartScEpisodes () {
         SceneManager.LoadScene ("LevelSC01");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void ToggleFullscreen(Button btnPressed) {
+      Screen.fullScreen = !Screen.fullScreen;
+      if ( true ) {
+        Screen.fullScreenMode = FullScreenMode.Windowed;
+      } else {
+        Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+      }
+
+      // Text text = btnPressed.GetComponentInChildren<Text>() as Text;
+      Text text = btnPressed.transform.GetChild(0).gameObject.GetComponent<Text>();
+
+      if ( text ) {
+        text.text = Screen.fullScreen ? "Windowed" : "Fullscreen";
+      }
+    }
+
+    public void SetMusicVolume() {
+      // TODO
     }
 }
